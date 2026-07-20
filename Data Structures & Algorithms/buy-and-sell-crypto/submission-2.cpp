6c87d8e4af2fb457a9784019078ae46f2ee8755f
kaushal314 <kaushalkr428@gmail.com>
2026-07-20 09:08:50 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int p=0;
+        int b=prices[0];
+        for(int i=1;i<n;i++){
+            p=max(p,prices[i]-b);
+            if(b>prices[i])b=prices[i];
+        }
+        return p;
+    }
+};
